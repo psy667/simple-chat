15f7b3f18d0db3275333c91819b19e073ac71369
psy667 <arseniyoguzov@gmail.com>
2019-03-20 00:54:17 +0300

    prototype

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({credentials: true, origin: true}));
+
+
 
 app.get('/', (req, res) => {
   res.sendfile('dist/index.html');
 });
 
-const database = {};
+const database = {messages: []};
 
-app.get('/new', (req, res) => {
+
+
+app.get('/addNew', (req, res) => {
     const user = req.query.user;
-    const text = req.query.text;
+    const text = req.query.message;
+
+    database.messages.push({user, text, time: Date.now()});
+    console.dir(JSON.stringify(database));
+    res.send('ok');
+});
+
+app.get('/getNew', (req, res) => {
+    const lastTime = req.query.lastTime;
+    const result = database.messages.filter(item => item.time > lastTime);
+    res.send(result);
+});
+
+app.get('/getAll', (req, res) => {
+  res.send(database);
 })
+
+
+
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="messages" id='messages'>
+      <div class="message" v-for='message in messages'>
+        [{{ message.user }}]: {{ message.text }}
+      </div>
+    </div>
+    <input v-model='message' @keypress.enter='sendMessage'>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'app',
+  data(){
+    return {
+      user: "user",
+      message: "",
+      messages: []
+    }
+  },
+  methods: {
+    sendMessage(){
+      const user = this.user;
+      const message = this.message;
+      this.message = '';
+      axios.get('http://localhost:3000/addNew', {
+        params: {user, message}})
+        .then(console.log('success'))
+        .catch(e => {
+          console.log(e)
+        })
+    },
+    getAll(){
+      axios.get('http://localhost:3000/getAll')
+        .then((response) => {
+          console.log(this);
+          this.messages = response.data.messages;
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    }
+  },
+  mounted(){
+    setInterval(this.getAll, 1000);
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,8 +57,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+.messages{
+  height: 300px;
+  overflow-y: scroll;
 }
 </style>
