f2e050b06fa27f37cd9d1300ff510a3b490331ee
psy667 <arseniyoguzov@gmail.com>
2019-03-20 12:53:23 +0300

    add color nicknames

--- server.js
+++ server.js
@@ -10,8 +10,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({credentials: true, origin: true}));
 
 
-const db = new Datastore({filename : 'messages'});
-db.loadDatabase();
+const mess = new Datastore({filename : 'messages'});
+mess.loadDatabase();
+
+const users = new Datastore({filename : 'users'});
+users.loadDatabase();
+
+const getColor = (name) => {
+  const colors = ['#ff5e5e', '#ffab5e', '#fff65e', '#b0f957', '#3affdb', '#7970ff'];
+  const index = name.split('').reduce((acc, cur) => acc+cur.charCodeAt(0), 0) % colors.length;
+  return colors[index];
+}
+
 
 app.get('/', (req, res) => {
   res.sendfile('dist/index.html');
@@ -22,19 +32,23 @@ app.post('/addNew', (req, res) => {
     const user = req.body.user;
     const text = req.body.message;
     const time = Date.now();
-    db.insert({user, text, time});
+    const color = getColor(user);
+    mess.insert({user, text, time, color});
     res.send('ok');
 });
 
 app.get('/getNew', (req, res) => {
     const lastTime = parseInt(req.query.lastTime);
-    db.find({ time: { $gt: lastTime } }, function (err, docs) {
+    mess.find({ time: { $gt: lastTime } }, function (err, docs) {
       docs.sort((a, b) => {
         return a.time - b.time;
       });
       res.send(docs);
     });
 });
+
+
+
 
 const port = 4000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
